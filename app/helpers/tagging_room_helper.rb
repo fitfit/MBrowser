13555a06bffda9519a90329016a6af64d8f96dc2
unknown <Shaun@.(none)>
2011-03-22 18:19:46 -0400

    Added auto complet,e worked on layout design

--- app/helpers/tagging_room_helper.rb
+++ app/helpers/tagging_room_helper.rb
@@ -1,2 +1,22 @@
 module TaggingRoomHelper
+  def tag_boxes_for(tags)
+    s=""
+    unless tags.nil?
+      tags.each do |t|
+        s += %{<div  class="droppable ui-widget-header" id="#{t.name}">
+          <div style="text-align:center">
+            <b>#{t.name}</b>
+
+          </div>
+          <div>
+            <span><br></span>
+          </div>
+        </div>
+        <div class="clear"></div>}
+      end
+    else
+      s += '<div><br></div>'
+    end
+    s.html_safe
+  end
 end
