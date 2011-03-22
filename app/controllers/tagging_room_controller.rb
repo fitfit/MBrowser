2326942e057f601c4d287e9e0eb04beddd0533f3
David Shaun Guay <dshaun.guay@fitfitsoftware.com>
2011-03-22 10:49:44 -0400

    Added random Tagging Room

--- app/controllers/tagging_room_controller.rb
+++ app/controllers/tagging_room_controller.rb
@@ -1,7 +1,9 @@
 class TaggingRoomController < ApplicationController
   def index
     @movie = nil
-    Movie.all.each do |m|
+    ms = Movie.all
+    r = rand(ms.count)
+    ms[r+1..-1].concat(ms[0..r]).each do |m|
       if m.tag_list.blank?
         @movie =m
         break
