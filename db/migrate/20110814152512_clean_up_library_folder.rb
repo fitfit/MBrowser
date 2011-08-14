c342fd8e44fd439800b22b2ffb88dbf2ce57198f
Daguaa <Shaun@.(none)>
2011-08-14 13:23:22 -0400

    Optimizing library folder

---
+++ db/migrate/20110814152512_clean_up_library_folder.rb
@@ -0,0 +1,26 @@
+class CleanUpLibraryFolder < ActiveRecord::Migration
+  def self.up
+    Movie.includes(:thumbnails).find_each do |m|
+      filepath = m.system_file.path
+      date = File.stat(filepath).mtime
+      dirpath = "public/library/#{date.year}-#{date.month}/#{m.system_file.name}/"
+      newfilepath = dirpath + m.system_file.name + "." + m.system_file.file_type
+      FileUtils.mkdir_p dirpath
+      FileUtils.mv(filepath,newfilepath)
+      m.system_file.path = newfilepath
+      m.system_file.save
+      m.thumbnails.each do |t|
+        t.system_files.each do |f|
+          newffilepath = dirpath + File.basename(f.path)
+          FileUtils.mv(f.path,newffilepath)
+          f.path = newffilepath
+          f.save
+        end
+      end
+
+    end
+  end
+
+  def self.down
+  end
+end
